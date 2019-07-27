e134785f2b6f177d7325d53813184742e8ecb458
BM835 <devdev@bm835.space>
2019-07-27 19:09:04 +1000

    retry added

--- main.rb
+++ main.rb
@@ -1,24 +1,29 @@
-puts "Input first number: "
-@n1 = gets.to_i
-puts "Input operator: "
-@op = gets.delete!("\n")
-puts "Input second number: "
-@n2 = gets.to_i
-
-@ans = 0
-case @op
-	when "+"
-		@ans = @n1+@n2
-	when "-"
-		@ans = @n1-@n2
-	when "*"
-		@ans = @n1*@n2
-	when "/"
-		@ans = @n1/@n2
-	when "^"
-		@ans = @n1**@n2
-	else
-		puts "You have not inputted operator. (required +/-/*//, found #{@op}"
-	end
-
-puts "#{@n1} #{@op} #{@n2} = #{@ans}"
+begin
+	puts "Input first number: "
+	@n1 = gets.to_i
+	puts "Input operator: "
+	@op = gets.delete!("\n")
+	puts "Input second number: "
+	@n2 = gets.to_i
+	
+	@ans = 0
+	case @op
+		when "+"
+			@ans = @n1+@n2
+		when "-"
+			@ans = @n1-@n2
+		when "*"
+			@ans = @n1*@n2
+		when "/"
+			@ans = @n1/@n2
+		when "^"
+			@ans = @n1**@n2
+		else
+			puts "You have not inputted operator. (required +/-/*//, found #{@op}"
+		end
+	
+	puts "#{@n1} #{@op} #{@n2} = #{@ans}"
+	
+	puts "Retry? (y/n)"
+	@retry = gets
+end while @retry == "y\n"
